d25e1f956dcc158270f74c42dbc294bc4fc9f59e
Nectar Hive | NECT <rkaya57.77@gmail.com>
2026-07-04 21:30:58 +0300

    add gltf asset loader

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 globalThis.THREE = THREE;
+globalThis.GLTFLoader = GLTFLoader;
 globalThis.loadModule = (url) => import(url);
 
 await import('/game-core.js');
