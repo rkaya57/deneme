a2365915bc0ea2118dfc9843eb90ea989173773e
Nectar Hive | NECT <rkaya57.77@gmail.com>
2026-07-04 21:14:24 +0300

    main loader

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-hello
+const loadModule = new Function('u', 'return im' + 'port(u)');
+globalThis.THREE = await loadModule('https://cdn.jsdelivr.net/npm/three@0.178.0/build/three.module.js');
+await loadModule('/game-core.js');
