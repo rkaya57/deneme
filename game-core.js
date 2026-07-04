448ec55f3d1f8b8b890f5bc25bbf04900b727437
Nectar Hive | NECT <rkaya57.77@gmail.com>
2026-07-04 23:24:19 +0300

    load animation core

--- game-core.js
+++ game-core.js
@@ -2,4 +2,5 @@ const THREE = globalThis.THREE;
 const ctx = { THREE };
 globalThis.gameCtx = ctx;
 await globalThis.loadModule('/simple.js');
+await globalThis.loadModule('/animation-core.js');
 await globalThis.loadModule('/simple3.js');
