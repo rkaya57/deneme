45bb5dd65802de2c74a122cc84f5cf796f5b9f77
Nectar Hive | NECT <rkaya57.77@gmail.com>
2026-07-04 23:11:52 +0300

    add skeleton animation clone support

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils.js';
 
 globalThis.THREE = THREE;
 globalThis.GLTFLoader = GLTFLoader;
+globalThis.SkeletonUtils = SkeletonUtils;
 globalThis.loadModule = (url) => import(url);
 
 await import('/game-core.js');
